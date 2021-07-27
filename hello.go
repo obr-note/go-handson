33dad5ef58550dea95930cc5bc832e3c879e3a4c
obr-note <k33mdukb9n@yahoo.co.jp>
2021-07-27 23:55:49 +0900

    add file

--- hello.go
+++ hello.go
@@ -1,50 +1,11 @@
 package main
 
 import (
-	"fyne.io/fyne"
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/theme"
-	"fyne.io/fyne/widget"
+  "fmt"
+  "hello"
 )
 
-type MyEntry struct {
-  widget.Entry
-  entered func(e *MyEntry)
-}
-
-func NewMyEntry(f func(e *MyEntry)) *MyEntry {
-  e := &MyEntry{}
-  e.ExtendBaseWidget(e)
-  e.entered = f
-  return e
-}
-
-func (e *MyEntry) KeyDown(key *fyne.KeyEvent) {
-  switch key.Name {
-    case fyne.KeyReturn, fyne.KeyEnter:
-      e.entered(e)
-    default:
-      e.Entry.KeyDown(key)
-  }
-}
-
 func main() {
-  a := app.New()
-  w := a.NewWindow("Hello")
-  l := widget.NewLabel("Hello Fyne!")
-  e := NewMyEntry(func(e *MyEntry) {
-    s := e.Text
-    e.SetText("")
-    l.SetText("you type '" + s + "'.")
-  })
-
-  w.SetContent(
-    widget.NewVBox(
-      l, e,
-    ),
-  )
-
-  a.Settings().SetTheme(theme.LightTheme())
-  w.Resize(fyne.NewSize(350, 250))
-  w.ShowAndRun()
+  name := hello.Input("type your name")
+  fmt.Println("Hello, " + name + "!!")
 }
---
+++ sample.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "hello"
+)
+
+func main() {
+  wt := func(f *os.File, s string) {
+    _, er := f.WriteString(s + "\n")
+    if er != nil {
+      panic(er)
+    }
+  }
+
+  fn := "data.txt"
+
+  f, er := os.OpenFile(fn, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
+  if er != nil {
+    panic(er)
+  }
+  defer f.Close()
+
+  fmt.Println("*** start ***")
+  wt(f, "*** start ***")
+  for {
+    s := hello.Input("type message")
+    if s == "" {
+      break
+    }
+    wt(f, s)
+  }
+  wt(f, "*** end ***\n\n")
+  fmt.Println("*** end ***")
+}
