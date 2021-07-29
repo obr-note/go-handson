7772da36ee997fbea989682a3667257cad4a9867
obr-note <k33mdukb9n@yahoo.co.jp>
2021-07-30 00:51:21 +0900

    5-11

--- hello.go
+++ hello.go
@@ -1,19 +1,34 @@
 package main
 
 import (
-  "github.com/PuerkitoBio/goquery"
+  "net/http"
+  "io/ioutil"
+  "fmt"
+  "encoding/json"
 )
   
 
 func main() {
-  p := "https://golang.org"
-  doc, er := goquery.NewDocument(p)
+  p := "https://tuyano-dummy-data.firebaseio.com/mydata.json"
+  re, er := http.Get(p)
+  if er != nil {
+    panic(er)
+  }
+  defer re.Body.Close()
+
+  s, er := ioutil.ReadAll(re.Body)
   if er != nil {
     panic(er)
   }
 
-  doc.Find("a").Each(func(n int, sel *goquery.Selection){
-    lk, _ := sel.Attr("href")
-    println(n, sel.Text(), "(", lk, ")")
-  })
+  var data []interface{}
+  er = json.Unmarshal(s, &data)
+  if er != nil {
+    panic(er)
+  }
+
+  for i, im := range data {
+    m := im.(map[string]interface{})
+    fmt.Println(i, m["name"].(string), m["mail"].(string), m["tel"].(string))
+  }
 }
