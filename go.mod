a8271d539bed3f49a5634f3aa289384ce3621d99
obr-note <k33mdukb9n@yahoo.co.jp>
2021-07-29 23:37:18 +0900

    5-5

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module hello
 
 go 1.16
 
-require fyne.io/fyne v1.4.3
+require fyne.io/fyne v1.4.3 // indirect
--- hello.go
+++ hello.go
@@ -1,11 +1,31 @@
 package main
 
 import (
+  "io/ioutil"
+  "os"
   "fmt"
-  "hello"
 )
+  
 
 func main() {
-  name := hello.Input("type your name")
-  fmt.Println("Hello, " + name + "!!")
+  rt := func(f *os.File) {
+    s, er := ioutil.ReadAll(f)
+    if er != nil {
+      panic(er)
+    }
+    fmt.Println(string(s))
+  }
+
+  fn := "data.txt"
+
+  f, er := os.OpenFile(fn, os.O_RDONLY, os.ModePerm)
+  if er != nil {
+    panic(er)
+  }
+
+  defer f.Close()
+
+  fmt.Println("<< start >>")
+  rt(f)
+  fmt.Println("<< end >>")
 }
